368f2bb7e3c4900ab34143d0f84e51d40dd08ed6
Lê Duy <dangleduy137@gmail.com>
2022-04-25 23:35:56 +0700

    fix

--- main/utilz/Constants.java
+++ main/utilz/Constants.java
@@ -10,14 +10,14 @@ public class Constants {
     }
 
     public static class PlayerConstant{
-        public static final int RUNNING1 = 0;
-        public static final int RUNNING2 = 1;
-        public static final int IDLE = 2;
+        public static final int IDLE = 0;
+        public static final int RUNNING1 = 1;
+        public static final int RUNNING2 = 2;
         public static final int JUMPING = 3;
         public static final int FALLING = 4;
-        public static final int TAKEHIT = 5;
-        public static final int ATTACKING1 = 6;
-        public static final int ATTACKING2 = 7;
+        public static final int ATTACKING1 = 5;
+        public static final int ATTACKING2 = 6;
+        public static final int TAKEHIT = 7;
         public static final int DEATH = 8;
 
         public static int GetSpritesAmount(int player_action){
